00149e761073d89fbbf6a16fed23f26b23b00fe4
Bruno Queiros <brunoofq@gmail.com>
2015-09-30 19:30:24 -0300

    Adjusts to router work on server

--- app/core-routes.js
+++ app/core-routes.js
@@ -1,51 +1,26 @@
-// var React = require('react/addons'),
-// ReactApp = React.createFactory(require('./components/App.react.js'));
-//
-// // import React from 'react';
-// // import Router,{Location} from 'react-router';
-//
-// let routes = require('./routes');
-// import createLocation from 'history/lib/createLocation'
-// import Router, { RoutingContext, match } from 'react-router'
-
-
 import React from 'react';
 import createLocation from 'history/lib/createLocation';
-import {RoutingContext, match} from 'react-router';
+import { RoutingContext, match} from 'react-router'
+import { renderToString } from 'react-dom/server'
 import routes from './routes';
-
 
 module.exports = function(app) {
 
-  app.use('/*', function (req, res) {
+  app.use(function (req, res) {
     let location = createLocation(req.url);
 
     match({routes, location }, (error, redirectLocation, renderProps) => {
       if (redirectLocation)
         res.redirect(301, redirectLocation.pathname + redirectLocation.search)
       else if (error)
-        res.send(500, error.message)
+        res.status(500).send(error.message);
       else if (renderProps == null)
-        res.send(404, 'Not found')
+        res.status(404).send('Not found');
       else {
-        res.send(React.renderToString(<RoutingContext {...renderProps}/>))
+        let reactHtml = renderToString(<RoutingContext {...renderProps}/>);
+        res.render('index.ejs', {reactOutput: reactHtml});
       }
     })
   });
 
-  // app.get('/', function(req, res){
-  //   // React.renderToString takes your component
-  //   // and generates the markup
-  //   var reactHtml = React.renderToString(ReactApp({}));
-  //   // Output html rendered by react
-  //   // console.log(myAppHtml);
-  //   res.render('index.ejs', {reactOutput: reactHtml});
-  // });
-  //
-  // //Route not found -- Set 404
-  // app.get('*', (req, res) => {
-  //   res.json({
-  //     'route': 'Sorry this page does not exist!'
-  //   });
-  // });
 };
