eb91b5718f9d8dd20734f38ab23be9fb06807ebc
Bruno Queiros <brunoofq@gmail.com>
2015-09-30 19:50:13 -0300

    Update routes

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
+import Home from './components/Home.react';
 import App from './components/App.react';
+import ChatRoom from './components/ChatRoom.react';
 
 const routes = (
   <Route path="/" component={App}>
-    <Route path="about" component={App}/>
+    <IndexRoute component={Home} />
+    <Route path="chat" component={ChatRoom}/>
   </Route>
 );
 
