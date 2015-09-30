e838b48b9348cad7594404db67ee6d9dec3b7712
Bruno Queiros <brunoofq@gmail.com>
2015-09-30 19:46:50 -0300

    Now App component it's a wrapper

--- app/components/App.react.js
+++ app/components/App.react.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import VideoSection from './VideoSection.react';
-import MessageSection from './MessageSection.react';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="ui grid">
-        <VideoSection />
-        <MessageSection />
+      <div>
+        {this.props.children}
       </div>
     )
   }
