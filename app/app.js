3c54aedae29264458a85a986f2d6cadb07aebab2
Bruno Queiros <brunoofq@gmail.com>
2015-09-30 08:22:44 -0300

    Use react router on client

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import App from './components/App.react';
 import Recognition from './utils/Recognition';
+import { Router, Route, Link } from 'react-router';
+import routes from './routes';
 
 React.render(
-  <App />,
+  (
+    <Router routes={routes}> />
+  ),
   document.getElementById('react')
 );
