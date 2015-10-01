7c1b86f2acd0f56a8da9c352045f5236ec28ac8e
Bruno Queiros <brunoofq@gmail.com>
2015-09-30 21:54:39 -0300

    Show a select with languages on Home component

--- app/components/Home.react.js
+++ app/components/Home.react.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
+import LanguageSelect from './LanguageSelect.react';
 
 class Home extends React.Component {
+
+  componentDidMount() {
+    localStorage.removeItem('language');
+  }
+
+  _onClick = (event) => {
+    const language = localStorage.getItem('language');
+
+    if (language === '' || language === null) {
+      event.preventDefault();
+    }
+  }
+
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/chat" activeClassName="active">ChatRoom</Link></li>
-        </ul>
+      <div className="ui form grid centered">
+        <div className="column five wide ui">
+          <LanguageSelect />
+
+          <Link
+            to="/chat"
+            className="large ui button fluid blue"
+            onClick={this._onClick}
+          >Enter</Link>
+        </div>
       </div>
     )
   }
